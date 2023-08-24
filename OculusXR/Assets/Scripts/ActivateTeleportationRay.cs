6ab3ab32a823b254948a041e74f632afeaea4fe4
play <88676086+Paulos8102@users.noreply.github.com>
2023-08-24 19:36:14 +0530

    Updation teleport code

--- OculusXR/Assets/Scripts/ActivateTeleportationRay.cs
+++ OculusXR/Assets/Scripts/ActivateTeleportationRay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
@@ -11,8 +12,12 @@ public class ActivateTeleportationRay : MonoBehaviour
 
     public InputActionProperty leftCancel;
 
+    public XRRayInteractor leftRay;
+
     void Update()
     {
-        leftTeleportation.SetActive(leftCancel.action.ReadValue<float>()==0 && leftActivate.action.ReadValue<float>() > 0.1f); //no teleport when grabbing objects
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid); //Random arguments
+        
+        leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>()==0 && leftActivate.action.ReadValue<float>() > 0.1f); //no teleport when grabbing objects
     }
 }
