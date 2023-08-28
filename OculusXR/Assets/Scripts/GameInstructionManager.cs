cf20b82a52044cd7bc8584f10a0d62dc446a5015
play <88676086+Paulos8102@users.noreply.github.com>
2023-08-28 18:14:22 +0530

    Menu appear

--- OculusXR/Assets/Scripts/GameInstructionManager.cs
+++ OculusXR/Assets/Scripts/GameInstructionManager.cs
@@ -2,11 +2,14 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem; //we need to listen to an action
 
 public class GameInstructionManager : MonoBehaviour
 {
+    public Transform head;
+    public float spawnDistance = 2.0f;
     public GameObject instr;
     public InputActionProperty showButton;
 
@@ -22,6 +25,13 @@ public class GameInstructionManager : MonoBehaviour
         if( showButton.action.WasPressedThisFrame())
         {
             instr.SetActive(!instr.activeSelf);
+
+            instr.transform.position =  head.position + new Vector3 (head.forward.x, 0, head.forward.z).normalized * spawnDistance;
         }
+
+        instr.transform.LookAt (new Vector3 (head.position.x, instr.transform.forward.y, head.position.z));
+
+        //instr.transform.forward *= -1;
+        //to flip the rotation of the menu
     }
 }
