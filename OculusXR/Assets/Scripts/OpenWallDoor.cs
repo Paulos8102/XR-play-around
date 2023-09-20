04cffbed119b6033d816ff28692d298e655772d1
play <88676086+Paulos8102@users.noreply.github.com>
2023-09-20 13:33:43 +0530

    Door Animate

--- OculusXR/Assets/Scripts/OpenWallDoor.cs
+++ OculusXR/Assets/Scripts/OpenWallDoor.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class OpenWallDoor : MonoBehaviour
 {
+    public float duration = 1.0f;
+    public float loweredHeight = 1.5f;
+
+    private bool lowerDoor = false;
+    private Vector3 raisedPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        raisedPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleDoorOpen()
     {
-        
+        StopAllCoroutines();
+
+        if (!lowerDoor)
+        {
+            Vector3 lowerPosition = raisedPosition + Vector3.down * loweredHeight;
+            StartCoroutine(MoveDoor(lowerPosition));
+        }
+        else
+        {
+            StartCoroutine(MoveDoor(raisedPosition));
+        }
+
+        lowerDoor = !lowerDoor;
+    }
+
+    IEnumerator MoveDoor(Vector3 targetPosition)
+    {
+        float timeElapsed = 0;
+        Vector3 startPosition = transform.position;
+
+        while(timeElapsed < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, timeElapsed / duration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
     }
 }
