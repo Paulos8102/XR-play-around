108029e53df6784544ae2708ce349b103c8f1a4f
play <88676086+Paulos8102@users.noreply.github.com>
2023-10-05 13:54:42 +0530

    Scripts

--- OculusXR/Assets/Scripts/UI/WristUI.cs
+++ OculusXR/Assets/Scripts/UI/WristUI.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class WristUI : MonoBehaviour
