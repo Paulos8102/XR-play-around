f9b8694bd3e7cb1571d9c756a590ea63523871e2
play <88676086+Paulos8102@users.noreply.github.com>
2023-10-06 11:35:18 +0530

    Added Colliders

--- OculusXR/Assets/Scripts/Gun/BulletScript.cs
+++ OculusXR/Assets/Scripts/Gun/BulletScript.cs
@@ -7,6 +7,12 @@ public class BulletScript : MonoBehaviour
 {
     public float bulletDamage = 10f; // Amount of damage the bullet deals
     public GameObject explosionPrefab; // Prefab of the explosion effect
+    //private GameObject bulletPrefab;
+    //
+    //private void Start()
+    //{
+    //    BulletScript bullet = bulletPrefab.GetComponent<Collider>();
+    //}
 
     public void OnTriggerEnter(Collider other)
     {
