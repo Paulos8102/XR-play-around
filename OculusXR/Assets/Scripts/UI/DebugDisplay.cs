0a31d6474198fcbd3b1861f1a93167a6288f0cc8
play <88676086+Paulos8102@users.noreply.github.com>
2023-10-06 16:16:24 +0530

    Update DebugScript

--- OculusXR/Assets/Scripts/UI/DebugDisplay.cs
+++ OculusXR/Assets/Scripts/UI/DebugDisplay.cs
@@ -6,7 +6,8 @@ using TMPro;
 
 public class DebugDisplay : MonoBehaviour
 {
-    Dictionary<string, string> debugLogs = new Dictionary<string, string>();
+    //Dictionary<string, string> debugLogs = new Dictionary<string, string>();
+    private List<string> debugLogs = new List<string>();
 
     public TextMeshProUGUI display;
 
@@ -20,29 +21,49 @@ public class DebugDisplay : MonoBehaviour
         Application.logMessageReceived -= HandleLog;
     }
 
+    //void HandleLog(string logString, string stackTrace, LogType type)
+    //{
+    //    if (type == LogType.Log)
+    //    {
+    //        string[] splitString = logString.Split(char.Parse(":"));
+    //        string debugKey = splitString[0];
+    //        string debugValue = splitString.Length > 1 ? splitString[1] : "";
+
+    //        if (debugLogs.ContainsKey(debugKey))
+    //            debugLogs[debugKey] = debugValue;
+    //        else
+    //            debugLogs.Add(debugKey, debugValue);
+    //    }
+
+    //    string displayText = "";
+    //    foreach (KeyValuePair<string, string> log in debugLogs)
+    //    {
+    //        if (log.Value == "")
+    //            displayText += log.Key + "\n";
+    //        else
+    //            displayText += log.Key + ": " + log.Value + "\n";
+    //    }
+
+    //    display.text = displayText;
+    //}
+
     void HandleLog(string logString, string stackTrace, LogType type)
     {
-        if (type == LogType.Log)
-        {
-            string[] splitString = logString.Split(char.Parse(":"));
-            string debugKey = splitString[0];
-            string debugValue = splitString.Length > 1 ? splitString[1] : "";
+        // Add the log message to the list
+        debugLogs.Add(logString);
 
-            if (debugLogs.ContainsKey(debugKey))
-                debugLogs[debugKey] = debugValue;
-            else
-                debugLogs.Add(debugKey, debugValue);
-        }
+        // Update the display text
+        UpdateDisplay();
+    }
 
+    // Method to update the display text
+    void UpdateDisplay()
+    {
         string displayText = "";
-        foreach (KeyValuePair<string, string> log in debugLogs)
+        foreach (string log in debugLogs)
         {
-            if (log.Value == "")
-                displayText += log.Key + "\n";
-            else
-                displayText += log.Key + ": " + log.Value + "\n";
+            displayText += log + "\n";
         }
-
         display.text = displayText;
     }
 }
