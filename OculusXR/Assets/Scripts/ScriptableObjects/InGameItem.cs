f2f47d7b28d076caf8730c0554424a3fdc9af25a
play <88676086+Paulos8102@users.noreply.github.com>
2023-10-19 15:05:44 +0530

    New scene

---
+++ OculusXR/Assets/Scripts/ScriptableObjects/InGameItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scritable Objects")]
+public class InGameItem : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
